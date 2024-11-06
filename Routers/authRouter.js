5ae55e13691ed306723085e247b83a65e432c9ca
Lamonaga <iw.borisowmail@yandex.ru>
2024-11-07 03:57:07 +0500

    add middleware login checkAuth

--- Routers/authRouter.js
+++ Routers/authRouter.js
@@ -5,7 +5,7 @@ import checkAuth from "../utils/checkAuth.js";
 const authRouter = new Router();
 
 authRouter.post("/registration", authController.registration);
-authRouter.post("/login", authController.login);
+authRouter.post("/login", checkAuth, authController.login);
 authRouter.get("/users", checkAuth, authController.getUsers);
 
 export default authRouter;
